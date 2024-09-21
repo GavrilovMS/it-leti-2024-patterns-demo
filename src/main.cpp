40bf76a058be9aee2866b77b2cff6d15ad4fa3a5
GavrilovMS <matveigavrilov23@gmail.com>
2024-09-21 06:07:28 +0000

    Using conan added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
 int main(int, char**){
-    std::cout << "Hello, from CallCenter!\n";
+    rapidjson::Document doc;
+    doc.SetObject();
+    doc.AddMember("test", 123, doc.GetAllocator());
+    rapidjson::StringBuffer buffer;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+
+    std::cout << buffer.GetString() << std::endl;
+    return 0;
 }
